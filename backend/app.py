585babe43a3916721be3bb4289f429df95b51a57
litninginmyeyes <119336811+litninginmyeyes@users.noreply.github.com>
2026-05-31 23:52:46 +0300

    for railway

--- backend/app.py
+++ backend/app.py
@@ -463,4 +463,5 @@ if __name__ == '__main__':
     init_db()
     print('База данных инициализирована')
     print('Сервер запущен: http://127.0.0.1:5000')
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
